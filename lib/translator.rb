094fe0599500c5f1f8979121867ee47104286c9f
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-26 23:59:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,21 +10,3 @@ def load_library(file_path)
   end
   result
 end
-
-def get_japanese_emoticon(file_path, english_emoticon)
-  original = load_library(file_path)
-  if original["get_emoticon"].include?(english_emoticon) 
-    original["get_emoticon"][english_emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
-
-def get_english_meaning(file_path, japanese_emoticon)
- library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
