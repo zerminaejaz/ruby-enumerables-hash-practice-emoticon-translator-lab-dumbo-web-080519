0abbf6c9ad059f6789a5918df1bfa32ba0c44965
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 00:02:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
 end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-  # code goes here	  library = load_library(file_path)
+  library = load_library(file_path)
   if library["get_meaning"].include?(japanese_emoticon) 
     library["get_meaning"][japanese_emoticon]
   else
