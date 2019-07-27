6dfead04e7a57e2b52edc1587a69cf85ba4e9e9c
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 00:00:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
 def load_library(file_path)
-  # code goes here	  library = YAML.load_file(file_path)
+  library = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
     result["get_meaning"][emoticons[1]] = meaning
