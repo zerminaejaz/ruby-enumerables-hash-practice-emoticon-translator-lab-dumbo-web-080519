656997c877653c282483d1ea0eef66e5af8fc94d
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 00:00:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,11 @@
 # require modules here
 require 'yaml'
 def load_library(file_path)
-  original = YAML.load_file(file_path)
-  final_hash = {"get_meaning" => {}, "get_emoticon" => {}}
-  original.each do |meaning, emoticons|
-    final_hash["get_meaning"][emoticons[1]] = meaning
-    final_hash["get_emoticon"][emoticons[0]] = emoticons[1]
+  # code goes here	  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   result
-end
+end	end
