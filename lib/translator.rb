24b415cfcebe14dcf7f752e8266cad04f8edaf3e
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 00:03:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,28 +1,28 @@
 # require modules here
 require 'yaml'
 def load_library(file_path)
-  library = YAML.load_file(file_path)
-  result = {"get_meaning" => {}, "get_emoticon" => {}}
-  library.each do |meaning, emoticons|
-    result["get_meaning"][emoticons[1]] = meaning
-    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  original = YAML.load_file(file_path)
+  final = {"get_meaning" => {}, "get_emoticon" => {}}
+  original.each do |meaning, emoticons|
+    final["get_meaning"][emoticons[1]] = meaning
+    final["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   result
 end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  library = load_library(file_path)
-  if library["get_emoticon"].include?(english_emoticon) 
-    library["get_emoticon"][english_emoticon]
+  original = load_library(file_path)
+  if original["get_emoticon"].include?(english_emoticon) 
+    original["get_emoticon"][english_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
 end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-  library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
+  original = load_library(file_path)
+  if original["get_meaning"].include?(japanese_emoticon) 
+    original["get_meaning"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
