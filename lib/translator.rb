ac7003acad921654d9f0cb69c47e2d3b065dcca8
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 00:01:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,3 +18,12 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
     "Sorry, that emoticon was not found"
   end
 end
+
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  # code goes here	  library = load_library(file_path)
+  if library["get_meaning"].include?(japanese_emoticon) 
+    library["get_meaning"][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
